4b5134a1f2e2440e14c42a55e887356d36d09a4f
seijuro <seijuro78@gmail.com>
2017-08-04 10:55:52 +0900

    created handler & toolrunner for processing API with thread

--- src/main/java/com/github/seijuro/common/IParser.java
+++
@@ -1,5 +0,0 @@
-package com.github.seijuro.common;
-
-public interface IParser {
-    void parse();
-}
---
+++ src/main/java/com/github/seijuro/common/scrap/publicdata/PublicDataAPIServices.java
@@ -0,0 +1,4 @@
+package com.github.seijuro.common.scrap.publicdata;
+
+public class PublicDataAPIServices {
+}
