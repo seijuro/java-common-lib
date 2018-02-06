ec537768673c39fefdc1ae5c7481f6953371f37d
seijuro <seijuro78@gmail.com>
2018-02-06 13:45:12 +0900

    implement 'ZipUtils'

--- src/main/java/com/github/seijuro/common/util/ZipUtils.java
+++ src/main/java/com/github/seijuro/common/util/ZipUtils.java
@@ -1,12 +1,15 @@
 package com.github.seijuro.common.util;
 
+import org.apache.commons.lang3.CharSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.nio.file.PathMatcher;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.file.*;
+import java.util.Enumeration;
+import java.util.Objects;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 public class ZipUtils {
@@ -42,5 +45,72 @@ public class ZipUtils {
      */
     public static boolean containsZipExtension(Path path) {
         return matcher.matches(path);
+    }
+    public static void unzip(Path src, Path destDir) {
+        unzip(src, destDir, null);
+    }
+
+    public static void unzip(Path src, Path destDir, Charset charset) {
+        assert Objects.nonNull(src) && Objects.nonNull(destDir);
+
+        boolean result;
+        String dest = destDir.toFile().getAbsolutePath();
+
+        if (!dest.endsWith(File.separator)) {   dest = dest + File.separator;   }
+
+        if (!(result = Files.exists(destDir))) {
+            LOG.debug("Creating directories ... dest : {}", destDir.toFile().getAbsolutePath());
+
+            destDir.toFile().mkdirs();
+        }
+
+        LOG.debug("Creating directories is done ... result : {}", result);
+
+        try {
+            ZipFile zipFile = Objects.nonNull(charset) ? new ZipFile(src.toFile(), charset) : new ZipFile(src.toFile());
+            Enumeration entries = zipFile.entries();
+
+            while (entries.hasMoreElements()) {
+                ZipEntry zipEntry = (ZipEntry)entries.nextElement();
+
+                if (!zipEntry.isDirectory()) {
+                    //  Log
+                    LOG.debug("entry : {}", zipEntry);
+
+                    String zipEntryName = zipEntry.getName();
+                    int pos = zipEntryName.lastIndexOf(File.separator);
+
+                    if (pos > 0) {
+                        String parent = dest + zipEntryName.substring(0, pos);
+                        Path parentPath = Paths.get(parent);
+
+                        if (!Files.exists(parentPath)) {
+                            parentPath.toFile().mkdirs();
+                        }
+                    }
+
+                    final int BufferSize = 4096;
+                    int readBytes;
+                    byte[] buf = new byte[BufferSize];
+                    InputStream is = zipFile.getInputStream(zipEntry);
+                    BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(dest + zipEntryName));
+
+                    while ((readBytes = is.read(buf)) > 0) {
+                        bos.write(buf, 0, readBytes);
+                    }
+
+                    bos.close();
+                    is.close();
+                }
+            }
+
+            zipFile.close();
+        }
+        catch (FileNotFoundException fnfexcp) {
+            fnfexcp.printStackTrace();
+        }
+        catch (IOException ioexcp) {
+            ioexcp.printStackTrace();
+        }
     }
 }
